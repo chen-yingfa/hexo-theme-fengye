7ed3dece48c383a751ce4b7ce7ace36296aaf211
Donny Chan <chenyingfa1999@qq.com>
2023-09-19 00:50:14 +0800

    bug fixes and more interactive UI

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,6 @@
 // mobile menu
 function registerMobileMenu() {
-  $("#open-menu").click(function () {
-    // set height auto
-    $("#menu-panel").css("height", "auto");
-    // set translate y 0
-    $("#menu-content").css(
-      "transform",
-      "translate(0, 0) rotate(0) skew(0) scaleX(1) scaleY(1)"
-    );
-    $("#open-menu").css("display", "none");
-    $("#close-menu").css("display", "block");
-  });
-
-  $("#close-menu").click(function () {
+  function closeMenu() {
     // set height 0
     $("#menu-panel").css("height", "0");
     // set translate y -100%
@@ -22,7 +10,23 @@ function registerMobileMenu() {
     );
     $("#open-menu").css("display", "block");
     $("#close-menu").css("display", "none");
-  });
+  }
+
+  function openMenu() {
+    // set height auto
+    $("#menu-panel").css("height", "auto");
+    // set translate y 0
+    $("#menu-content").css(
+      "transform",
+      "translate(0, 0) rotate(0) skew(0) scaleX(1) scaleY(1)"
+    );
+    $("#open-menu").css("display", "none");
+    $("#close-menu").css("display", "block");
+  }
+
+  $("#open-menu").click(openMenu);
+  $("#close-menu").click(closeMenu);
+  $(".mask").click(closeMenu);
 }
 
 // header page title
